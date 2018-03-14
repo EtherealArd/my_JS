68eb52ebb60030a2d19ff5497410b2a4b9312b0b
wit20ger33grineta24xaradas77 <vaultec114@gmail.com>
2018-03-15 01:44:29 +0200

    nodelab

--- JSTasks/NodeAuth/main.js
+++ JSTasks/NodeAuth/main.js
@@ -1,7 +1,8 @@
+
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var bodyParser = require('body-parser');
 
 
 app.use('/', express.static(path.join(__dirname ,'/styles')));
@@ -10,12 +11,74 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post("/",function(req,res){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-console.log(req.body);
+app.use(bodyParser.json());
+
+app.post("/lul", function(req,res){
+var arr = req.body;
+var maxVal = maxValue(arr);
+res.send(maxVal.toString());
 });
 
+app.post("/azaza", function (req, res) {
+   var valid = new Validator(req.body);
+
+  var responseData = valid.validate();
+});
+
+function maxValue(arr)
+{
+
+    var str = JSON.stringify(arr);
+    var chars = str.split(',');   
+    var number = [];     
+    console.log(chars);
+    for(var i=0;i<chars.length;i++)
+    {
+        console.log(chars[i]);
+      number.push(parseInt(chars[i]));             
+    }
+    max = number[0];
+    console.log(number);
+    for(var i=0;i<number.length;i++)
+   {       
+      if(number[i]>max)
+      max = number[i];
+    }
+    return max;
+
+}
 
 
+
+
+function Validator(formData)
+{
+    this.name = formData.fname;
+    this.lname = formData.lname;
+    this.city = formData.city;
+    this.index = formData.index;
+    this.email = formData.email;
+    this.passwords = formData.password;
+    this.namePattern = /^[a-zA-Zа-яА-Я]{0,25}$/;
+    this.fnamePattern = /^[a-zA-Zа-яА-Я]{0,25}$/i;
+    this.cityPattern = /^[a-zA-Zа-яА-Я]{0,25}$/i;
+    this.indexPattern = /^[0-9]{5,5}$/i;
+    this.emailPattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
+}
+
+Validator.prototype.validate = function()
+{
+var dataRespones = "";
+dataRespones += this.namePattern.test(this.name)?this.name+" - is valid " : this.name+" - is not valid ";
+dataRespones += this.fnamePattern.test(this.lname)?this.lname+" - is valid " : this.lname+" - is not valid ";
+dataRespones += this.cityPattern.test(this.city)?this.city+" - is valid " : this.city+" - is not valid ";
+dataRespones += this.indexPattern.test(this.index)?this.index+" - is valid " : this.index+" - is not valid ";
+dataRespones += this.emailPattern.test(this.email)?this.email+" - is valid" : this.email+" - is not valid ";
+return dataRespones;
+}
 
 app.listen(8080);
